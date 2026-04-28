b46193cea12066676dbc9d6628abb1a296b68e31
kektola70-sketch <kektola70@gmail.com>
2026-04-28 13:38:53 +0300

    update main.js

--- main.js
+++ main.js
@@ -2,12 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
+    width: 1280, height: 720,
+    webPreferences: { nodeIntegration: true, contextIsolation: false }
   });
   win.loadFile('index.html');
 }
